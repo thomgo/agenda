d331f8e0ca8b7d151d0bb90b21b1caa86f3b6cba
Thomas <tg.59290@gmail.com>
2019-09-18 10:22:47 +0200

    display right calendar and current date on main

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 # coding: utf-8
 import os
 import time
+import calendar
+import locale
+from datetime import datetime
 
 from view.eventView import eventView
 
 # Main file acting like a routing system
 # Call the right method from the view, depending on user input
 
-
 # Simple intro for the app
 os.system('cls' if os.name == 'nt' else 'clear')
 print("Bienvenue sur votre agenda personnel")
 time.sleep(3)
+
+# Set locale zone to France and get the current month and year to display right calendar
+locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+currentYear = datetime.now().year
+currentMonth = datetime.now().month
 
 # The action the user wants to do, by default nothing
 action = ""
 # while the user does not chose to leave the program
 while action != 'q':
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Que souhaitez vous gérez ? (c: conférences, i: intervenants, q: quitter)")
+    print("Nous sommes le : {}".format(datetime.today().strftime('%d %B %Y')))
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    print (calendar.month(currentYear, currentMonth, 2, 1))
+    print("Que souhaitez vous gérez ? (q: quitter)")
     action = input(": ")
     # call the right action function according to user input
 
